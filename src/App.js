3e5262ab36df82ccf54034b98a472fd754efd5df
Daniel <dancardosobr@gmail.com>
2020-07-17 23:44:14 +0100

    chore: add bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Routes from './routes';
 
 function App() {
