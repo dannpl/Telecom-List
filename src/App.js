5aa98a48cef661a132cc3ff0c3ee996f6f0eefc8
Daniel <dancardosobr@gmail.com>
2020-07-19 07:29:57 +0100

    chore: deploy

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
-import Header from './components/Header';
+import Header from './cp/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
